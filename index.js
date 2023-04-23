0c6e62a4e420c556f94cac45b718df22c21d2f15
Mmackz <matthewmackenz@gmail.com>
2023-04-23 14:15:25 -0700

    refactor code

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const discord = require("./lib/discord");
 
 const { client } = discord;
 
+// log in and start listening for QuestCreated Events
 client.once(Events.ClientReady, async (c) => {
-   // log in and start listening for QuestCreated Events
    console.log(`Ready! Logged in as ${c.user.tag}`);
    subscribeEvents(discord.sendMessage);
 });
 
+// setup channels/roles when installed to new server
 client.on(Events.GuildCreate, async (guild) => {
    await discord.setupGuild(guild);
 });
 
+// assign/remove Quester role when button is clicked
 client.on("interactionCreate", async (interaction) => {
    if (interaction.isButton() && interaction.customId === "set-role") {
       await discord.toggleRole(interaction);
